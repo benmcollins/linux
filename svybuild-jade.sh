d84419548bf5fa09f792744fba915c050946efc8
Ben Collins <ben.c@servergy.com>
2013-11-15 11:31:12 -0500

    SVY: Update build scripts to accomodate cross compile environments
    Signed-off-by: Ben Collins <ben.c@servergy.com>

--- svybuild-jade.sh
+++ svybuild-jade.sh
@@ -6,6 +6,18 @@ nway=`echo "($cpus * 15) / 10" | bc`
 dir="`pwd`/build-jade"
 export MAKEFLAGS="O=$dir -j$nway"
 
+buildarch="`uname -p`"
+
+if [ "$buildarch" != "ppc" ]; then
+	if [ -d /opt/fsl-networking/QorIQ-SDK-V1.4/sysroots/x86_64-fsl_networking_sdk-linux ]; then
+		rootdir="/opt/fsl-networking/QorIQ-SDK-V1.4/sysroots/x86_64-fsl_networking_sdk-linux"
+	else
+		rootdir="/opt/fsl-networking/QorIQ-SDK-V1.4/sysroots/i686-fsl_networking_sdk-linux"
+	fi
+	export PATH="$PATH:$rootdir/usr/bin/ppce500mc-fsl_networking-linux"
+	export MAKEFLAGS="$MAKEFLAGS CROSS_COMPILE=powerpc-fsl_networking-linux- ARCH=powerpc"
+fi
+
 test -d "$dir" || mkdir "$dir"
 
 test -f "$dir/.config" || cp svy-configs/svy-jade "$dir/.config"
--- svybuild-yoda.sh
+++ svybuild-yoda.sh
@@ -6,6 +6,18 @@ nway=`echo "($cpus * 15) / 10" | bc`
 dir="`pwd`/build-yoda"
 export MAKEFLAGS="O=$dir -j$nway"
 
+buildarch="`uname -p`"
+
+if [ "$buildarch" != "ppc" ]; then
+	if [ -d /opt/fsl-networking/QorIQ-SDK-V1.4/sysroots/x86_64-fsl_networking_sdk-linux ]; then
+		rootdir="/opt/fsl-networking/QorIQ-SDK-V1.4/sysroots/x86_64-fsl_networking_sdk-linux"
+	else
+		rootdir="/opt/fsl-networking/QorIQ-SDK-V1.4/sysroots/i686-fsl_networking_sdk-linux"
+	fi
+	export PATH="$PATH:$rootdir/usr/bin/ppc64e6500-fsl_networking-linux"
+	export MAKEFLAGS="$MAKEFLAGS CROSS_COMPILE=powerpc64-fsl_networking-linux- ARCH=powerpc"
+fi
+
 test -d "$dir" || mkdir "$dir"
 
 test -f "$dir/.config" || cp svy-configs/svy-yoda "$dir/.config"
