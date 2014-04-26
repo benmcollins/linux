8d0b7e3601f647fdadd465a83b9c1dfb2aee96b1
Ben Collins <ben.c@servergy.com>
2014-04-25 22:19:13 -0500

    SVY: build: Update yoda config and builds scripts
    Signed-off-by: Ben Collins <ben.c@servergy.com>

--- svybuild-yoda.sh
+++ svybuild-yoda.sh
@@ -1,25 +1,29 @@
-#!/bin/sh
+#!/bin/bash
+
+target=yoda
 
 cpus=`getconf _NPROCESSORS_ONLN`
 nway=`echo "($cpus * 15) / 10" | bc`
 
-dir="`pwd`/build-yoda"
-export MAKEFLAGS="O=$dir -j$nway"
+dir="`pwd`/build-$target"
+export MAKEFLAGS="O=$dir -j$nway ARCH=powerpc"
 
-buildarch="`uname -p`"
-
-if [ "$buildarch" != "ppc" -a "$buildarch" != "ppc64" ]; then
+if type -p powerpc-linux-gnu-gcc; then
+	export MAKEFLAGS="$MAKEFLAGS CROSS_COMPILE=powerpc-linux-gnu-"
+else
+	# Cross compiling?
 	if [ -d /opt/fsl-networking/QorIQ-SDK-V1.4/sysroots/x86_64-fsl_networking_sdk-linux ]; then
 		rootdir="/opt/fsl-networking/QorIQ-SDK-V1.4/sysroots/x86_64-fsl_networking_sdk-linux"
 	else
 		rootdir="/opt/fsl-networking/QorIQ-SDK-V1.4/sysroots/i686-fsl_networking_sdk-linux"
 	fi
-	export PATH="$PATH:$rootdir/usr/bin/ppc64e6500-fsl_networking-linux"
-	export MAKEFLAGS="$MAKEFLAGS CROSS_COMPILE=powerpc64-fsl_networking-linux- ARCH=powerpc"
+	export PATH="$PATH:$rootdir/usr/bin/ppce500mc-fsl_networking-linux"
+	export MAKEFLAGS="$MAKEFLAGS CROSS_COMPILE=powerpc64-fsl_networking-linux-"
 fi
 
-test -d "$dir" || mkdir "$dir"
-
-test -f "$dir/.config" || cp svy-configs/svy-yoda "$dir/.config"
+if ! test -f "$dir/.config"; then
+	test -d "$dir" || mkdir "$dir"
+	cp "svy-configs/svy-$target" "$dir/.config"
+fi
 
 make uImage
