ddffd02f92f0fd7acda86ea95502614c02884862
Ben Collins <ben.c@servergy.com>
2015-09-14 08:39:00 -0500

    SVY: gfs2: Change gfs2_scan_glock_lru() to return unsigned long
    The signed long was causing an overflow on 64-bit filesystems.
    
    Signed-off-by: Ben Collins <ben.c@servergy.com>

--- fs/gfs2/glock.c
+++ fs/gfs2/glock.c
@@ -1427,12 +1427,12 @@ __acquires(&lru_lock)
  * gfs2_dispose_glock_lru() above.
  */
 
-static long gfs2_scan_glock_lru(int nr)
+static unsigned long gfs2_scan_glock_lru(int nr)
 {
 	struct gfs2_glock *gl;
 	LIST_HEAD(skipped);
 	LIST_HEAD(dispose);
-	long freed = 0;
+	unsigned long freed = 0;
 
 	spin_lock(&lru_lock);
 	while ((nr-- >= 0) && !list_empty(&lru_list)) {
